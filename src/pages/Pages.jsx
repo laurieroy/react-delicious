afb89faa583b700ea84511458911e1fba805ff4f
Laurie Roy <laurieroy.dev@gmail.com>
2022-09-23 20:31:12 -0700

    Wire up RecipeDetails

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
+import RecipeDetails from "./RecipeDetails";
 
 function Pages() {
   return (
@@ -9,6 +10,7 @@ function Pages() {
       <Route exact path="/" element={<Home />} />
       <Route path="/cuisine/" element={<Cuisine />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/recipes/:id" element={<RecipeDetails />} />
       <Route path="/searched/:search" element={<Searched />} />
     </Routes>
   );
---
+++ src/pages/RecipeDetails.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+function RecipeDetails() {
+  let params = useParams();
+  const baseUrl = "https://api.spoonacular.com/recipes/";
+  const key = process.env.REACT_APP_API_KEY;
+  const [activeTab, setActiveTab] = useState("instuctions");
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `${baseUrl}/${params.name}/information?apiKey=${key}`
+    );
+    const detailData = await data.json();
+    setDetails(detailData);
+    console.log(detailData);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name, fetchDetails]);
+
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+      <Info>
+        <Button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients
+        </Button>
+        <>
+          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+          <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+        </>
+      </Info>
+    </DetailWrapper>
+  );
+}
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  h2 {
+    margin-bottom: 2rem;
+  }
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
+
+export default RecipeDetails;
