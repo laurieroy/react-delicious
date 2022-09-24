443efb08707daaa19ca24e12b128a5bd71a173b3
Laurie Roy <laurieroy.dev@gmail.com>
2022-09-23 19:46:20 -0700

    Implement display search results

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,8 +1,56 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Searched() {
-	return (
-		<div>Searched</div>
-	)
+  let params = useParams();
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+  const getSearched = async (query) => {
+    const api = `https://api.spoonacular.com/recipes/`;
+    const key = process.env.REACT_APP_API_KEY;
+    const data = await fetch(
+      `${api}complexSearch?apiKey=${key}&query=${query}`
+    );
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
+  return (
+    <Grid>
+      {searchedRecipes.map((recipe) => {
+        return (
+          <Card key={recipe.id}>
+            <img src={recipe.image} alt={recipe.title} />
+            <h4>{recipe.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
 
-export default Searched
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    border-radius: 2rem;
+    width: 100%;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
+export default Searched;
