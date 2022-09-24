f362f5e555458c5ce0022213c70529fdbd8e5a2a
Laurie Roy <laurieroy.dev@gmail.com>
2022-09-23 20:40:14 -0700

    Add display ingredients to detail page

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -41,10 +41,20 @@ function RecipeDetails() {
         >
           Ingredients
         </Button>
-        <>
-          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
-          <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
-        </>
+
+        {activeTab === "instructions" && (
+          <>
+            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+          </>
+        )}
+        {activeTab === "ingredients" && (
+          <ul>
+            {details.extendedIngredients.map((ingredient) => {
+              <li key={ingredient.id}>{ingredient.original}</li>;
+            })}
+          </ul>
+        )}
       </Info>
     </DetailWrapper>
   );
