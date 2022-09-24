b07b7aa3dafec88a35f94b51e4e4ac78e888edb8
Laurie Roy <laurieroy.dev@gmail.com>
2022-09-23 18:50:03 -0700

    Add styling to menu, active

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,32 +4,68 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 function Category() {
-	return (
-		<List>
-			<NavLink to={"/cuisine/italian"}>
-				<FaPizzaSlice />
-				<h4>Italian</h4>
-			</NavLink>
-			<NavLink to={"/cuisine/american"}>
-				<FaHamburger />
-				<h4>American</h4>
-			</NavLink>
-			<NavLink to={"/cuisine/thai"}>
-				<GiNoodles />
-				<h4>Thai</h4>
-			</NavLink>
-			<NavLink to={"/cuisine/japanese"}>
-				<GiChopsticks />
-				<h4>Japanese</h4>
-			</NavLink>
-		</List>
-	)
+  return (
+    <List>
+      <SLink to={"/cuisine/italian"}>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </SLink>
+      <SLink to={"/cuisine/american"}>
+        <FaHamburger />
+        <h4>American</h4>
+      </SLink>
+      <SLink to={"/cuisine/thai"}>
+        <GiNoodles />
+        <h4>Thai</h4>
+      </SLink>
+      <SLink to={"/cuisine/japanese"}>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </SLink>
+    </List>
+  );
 }
 
 const List = styled.div`
-	display: flex;
-	justify-content: center;
-	margin: 2rem 0rem;
-`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
+const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale() (0.8);
 
-export default Category
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+
+	svg {
+		color: white;
+    font-size: 1.5rem;
+	}
+
+	&.active{
+		background: linear-gradient(to right, #f27121, #e94057);
+		
+		h4 {
+    color: white;
+  	}
+
+		svg {
+			color: white;
+		}
+	}
+`;
+
+export default Category;
