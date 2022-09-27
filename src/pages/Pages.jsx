042a4cdf98234abc7c61fbccddba0bae27acc8c4
Laurie Roy <laurieroy.dev@gmail.com>
2022-09-27 08:36:44 -0700

    Add animation to Home and Cuisine

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import RecipeDetails from "./RecipeDetails";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/cuisine/:type" element={<Cuisine />} />
-      <Route path="/recipe/:id" element={<RecipeDetails />} />
-      <Route path="/searched/:search" element={<Searched />} />
-    </Routes>
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/searched/:search" element={<Searched />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
